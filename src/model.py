5ab8861b17cc6cb79d76a0cd166fb4fdbc69268b
Szabo Zsombor <szazs444@gmail.com>
2025-08-11 15:57:40 +0200

    add: adding starting conv dqn model

---
+++ src/model.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
+
+class DQNNetwork(nn.Module):
+
+    def __init__(self, action_space_size):
+        nn.Module.__init__(self)
+        torch.random.manual_seed(0)
+
+        self.conv1 = nn.Conv2d(4, 32, kernel_size=8, stride=4)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.fc1 = nn.Linear(64 * 9 * 9, 512)
+        self.fc2 = nn.Linear(512, action_space_size)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.relu(self.bn2(self.conv2(x)))
+        x = x.view(x.size(0), -1)
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
