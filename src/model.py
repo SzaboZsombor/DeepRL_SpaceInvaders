4448e1e9fe7d9824332bf6050c8ae95b940df4ee
Szabó Zsombor <szazs444@gmail.com>
2025-08-12 00:38:21 +0200

    feat: updated model to Dueling

--- src/model.py
+++ src/model.py
@@ -5,31 +5,42 @@ import torch.nn.functional as F
 from utils import get_model_path
 
 
-class DQNNetwork(nn.Module):
+class DuelingDQNNetwork(nn.Module):
 
     def __init__(self, action_space_size):
-        nn.Module.__init__(self)
+        super(DuelingDQNNetwork, self).__init__()
         torch.random.manual_seed(0)
 
         self.conv1 = nn.Conv2d(4, 32, kernel_size=8, stride=4)
         self.bn1 = nn.BatchNorm2d(32)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
         self.bn2 = nn.BatchNorm2d(64)
-        self.fc1 = nn.Linear(64 * 9 * 9, 512)
-        self.fc2 = nn.Linear(512, action_space_size)
+
+        self.value_fc1 = nn.Linear(64 * 9 * 9, 512)
+        self.value_fc2 = nn.Linear(512, action_space_size)
+
+        self.advantage_fc1 = nn.Linear(64 * 9 * 9, 512)
+        self.advantage_fc2 = nn.Linear(512, action_space_size)
+
         self.relu = nn.ReLU()
 
     def forward(self, x):
         x = self.relu(self.bn1(self.conv1(x)))
         x = self.relu(self.bn2(self.conv2(x)))
         x = x.view(x.size(0), -1)
-        x = self.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
+
+        value = self.relu(self.value_fc1(x))
+        value = self.value_fc2(value)
+
+        advantage = self.relu(self.advantage_fc1(x))
+        advantage = self.advantage_fc2(advantage)
+
+        q_vals = value + (advantage - advantage.mean(dim=1, keepdim=True))
+        return q_vals
     
     def save_model_weights(self, file_name):
         file_path = get_model_path(file_name)
-        
+
         torch.save(self.state_dict(), file_path)
         print(f"Model weights saved to {file_path}")
 
