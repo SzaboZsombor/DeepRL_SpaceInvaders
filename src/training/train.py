dfe2c88dd69faba945852a6db1db12bfe739d0fe
Szabó Zsombor <szazs444@gmail.com>
2025-08-14 11:18:39 +0200

    feat: updated hyperparameters and improved training process

--- src/training/train.py
+++ src/training/train.py
@@ -12,7 +12,7 @@ from plot import plot_training_progress
 from utils import get_logs_dir
 
 
-with open("config.yaml", "r") as f:
+with open(os.path.join(os.path.dirname(__file__), "config.yaml"), "r") as f:
     config = yaml.safe_load(f)
 
 LEARNING_RATE = config["LEARNING_RATE"]
@@ -27,13 +27,13 @@ CAPACITY = config["CAPACITY"]
 env = environment.SpaceInvadersEnv()
 
 agent = agent.Agent(action_space_size=env.action_space.n, learning_rate=LEARNING_RATE, gamma=GAMMA, tau=TAU, batch_size=BATCH_SIZE, capacity=CAPACITY)
-agent.local_model.load_model_weights("best_model.pth")
+agent.local_model.load_model_weights("best_ddqn_agent.pth")
 
-def get_eps(step, decay_rate=0.999999, min_eps=0.1, starting_eps=1.0):
+def get_eps(step, decay_rate, min_eps, starting_eps):
     return max(min_eps, starting_eps * (decay_rate ** step))
 
 
-def train_agent(episodes=1000, max_steps=10000, weights_output_name="best_model.pth"):
+def train_agent(episodes=10000, max_steps=10000, weights_output_name="best_ddqn_agent.pth"):
     best_score = -np.inf
 
     if os.path.exists(f"{get_logs_dir()}/training_scores.npy"):
@@ -41,14 +41,14 @@ def train_agent(episodes=1000, max_steps=10000, weights_output_name="best_model.
     else:
         scores = []
 
-    for episode in trange(episodes, desc=f"Training Progress"):
+    for episode in trange(len(scores), episodes, desc="Training Progress"):
 
         state, _ = env.reset()
         total_reward = 0
 
         for t in range(max_steps):
 
-            action = agent.act(state, epsilon=get_eps(agent.time_step))
+            action = agent.act(state, epsilon=get_eps(agent.time_step, decay_rate=DECAY_RATE, min_eps=MIN_EPS, starting_eps=STARTING_EPS))
 
             next_state, reward, terminated, truncated, _ = env.step(action)
 
@@ -67,17 +67,19 @@ def train_agent(episodes=1000, max_steps=10000, weights_output_name="best_model.
             agent.local_model.save_model_weights(weights_output_name)
 
         scores.append(total_reward)
-        print(f"Episode {episode + 1}/{episodes} - Total Reward: {total_reward}")
+        print(f" Episode {episode + 1}/{episodes} - Total Reward: {total_reward}", flush=True)
 
         np.save(f"{get_logs_dir()}/training_scores.npy", np.array(scores))
 
 
 def main():
     print("Starting training...")
+    episodes = 10000
+    max_steps = 10000
 
-    weights_output_name = "best_model.pth"
+    weights_output_name = "best_ddqn_agent.pth"
 
-    train_agent(weights_output_name=weights_output_name)
+    train_agent(episodes=episodes, max_steps=max_steps, weights_output_name=weights_output_name)
 
     scores = np.load(f"{get_logs_dir()}/training_scores.npy")
     plot_training_progress(scores)
