307632dc8364be23207aecf48c0122ba55a0c8f2
Szabo Zsombor <szazs444@gmail.com>
2025-08-16 16:22:30 +0200

    add: adding code to watch the agent play with trained weights

--- src/environment.py
+++ src/environment.py
@@ -27,28 +27,3 @@ class SpaceInvadersEnv:
     def close(self):
         print("Closing environment")
         return self.env.close()
-
-
-
-if __name__ == '__main__':
-    # Initialize the environment with a render mode to see the game
-    env_manager = SpaceInvadersEnv(render_mode='human')
-    
-    # Get the initial state of the environment
-    observation, info = env_manager.reset()
-    
-    done = False
-    truncated = False
-    total_reward = 0
-    
-    # Run a simple loop with random actions
-    print("Starting a short test run with random actions...")
-    while not done and not truncated:
-        action = env_manager.action_space.sample()  # Take a random action
-        observation, reward, done, truncated, info = env_manager.step(action)
-        total_reward += reward
-
-        print(observation)
-        
-    print(f"Test run finished. Total reward: {total_reward}")
-    env_manager.close()
